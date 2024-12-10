56c139937a217741f4ea0dacfda23feb9aecbab0
May-xxvi98 <shielamaydevega98@gmail.com>
2024-12-10 13:57:47 +0000

    m

---
+++ wah.java
@@ -0,0 +1,10 @@
+public class wah{
+		
+		public static void main(String args[]){
+            char c[] = new char[]{'a','b','c'};
+			String cd = "abcdef".substring(4);
+			String s1 = new String(c);
+			s1 += cd;
+			System.out.print(s1);
+		}
+	}         				
